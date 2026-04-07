0fe503f29d58a434c8bc180f5bfa64f2b83d6b5c
Studio JK <devstudiojk@gmail.com>
2026-04-07 04:56:09 -0300

    COLOQUEI O JOGO COM INTERFACE, JOGAVEL
    Sendo apenas o primeiro projeto de muitos, esse jogo já vem desafiando minhas habilidades, sendo feito do 0!

--- GAME.c
+++ GAME.c
@@ -1,262 +1,246 @@
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
 
-#define NUM_CONTINENTES 5
-#define PAISES_POR_CONTINENTE 5
+#define TOTAL_PAISES 25
 #define MAX_JOGADORES 5
 
-struct Pais {
+typedef struct {
     char nome[30];
     int dono;
     int tropas;
-};
+    SDL_Rect retangulo;
+} Pais;
 
-struct Continente {
+typedef struct {
     char nome[30];
-    struct Pais paises[PAISES_POR_CONTINENTE];
-};
+    SDL_Color cor;
+} Jogador;
 
-struct Jogador {
-    char nome[30];
-};
-
-// ======= paises =======
-const char* nomesPaises[NUM_CONTINENTES][PAISES_POR_CONTINENTE] = {
-    {"Brasil", "EUA", "Canada", "Mexico", "Argentina"},
-    {"Franca", "Alemanha", "Italia", "Espanha", "Inglaterra"},
-    {"China", "Japao", "India", "Coreia", "Russia"},
-    {"Egito", "Nigeria", "AfricaSul", "Marrocos", "Quenia"},
-    {"Australia", "NovaZelandia", "Fiji", "Samoa", "Tonga"}
-};
-
-// ================= ui =================
-void mostrarPorJogador(struct Continente mapa[], struct Jogador jogadores[], int numJogadores) {
-    printf("\n====== TERRITORIOS POR JOGADOR ======\n");
-
-    for (int j = 0; j < numJogadores; j++) {
-        printf("\n%s:\n", jogadores[j].nome);
-
-        for (int i = 0; i < NUM_CONTINENTES; i++) {
-            for (int k = 0; k < PAISES_POR_CONTINENTE; k++) {
-                if (mapa[i].paises[k].dono == j) {
-                    printf("- %s (%s) | Tropas: %d\n",
-                        mapa[i].paises[k].nome,
-                        mapa[i].nome,
-                        mapa[i].paises[k].tropas);
-                }
-            }
-        }
-    }
+void escreverTexto(SDL_Renderer* r, TTF_Font* f, const char* t, int x, int y, SDL_Color c){
+    SDL_Surface* s = TTF_RenderText_Blended(f,t,c);
+    if (!s) return;
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(r,s);
+    SDL_Rect dst = {x,y,s->w,s->h};
+    SDL_RenderCopy(r,tex,NULL,&dst);
+    SDL_FreeSurface(s);
+    SDL_DestroyTexture(tex);
 }
 
-// ================= mapa =================
-void mostrarMapa(struct Continente mapa[], struct Jogador jogadores[]) {
-    printf("\n========== MAPA GLOBAL ==========\n");
-
-    for (int i = 0; i < NUM_CONTINENTES; i++) {
-        printf("\n[%d] %s\n", i, mapa[i].nome);
-
-        for (int j = 0; j < PAISES_POR_CONTINENTE; j++) {
-            printf("  (%d) %-15s | %-10s | Tropas: %d\n",
-                j,
-                mapa[i].paises[j].nome,
-                jogadores[mapa[i].paises[j].dono].nome,
-                mapa[i].paises[j].tropas);
-        }
-    }
+int mouseSobre(SDL_Rect r,int mx,int my){
+    return mx>=r.x && mx<=r.x+r.w && my>=r.y && my<=r.y+r.h;
 }
 
-// ================= Territorios de cada jogador =================
-void mostrarTerritoriosJogador(struct Continente mapa[], int jogadorAtual) {
-    printf("\nSeus territorios:\n");
-
-    for (int i = 0; i < NUM_CONTINENTES; i++) {
-        for (int j = 0; j < PAISES_POR_CONTINENTE; j++) {
-            if (mapa[i].paises[j].dono == jogadorAtual) {
-                printf("- %s (%s) | Tropas: %d\n",
-                    mapa[i].paises[j].nome,
-                    mapa[i].nome,
-                    mapa[i].paises[j].tropas);
-            }
-        }
-    }
+void rolarDados(int dados[], int qtd){
+    for(int i=0;i<qtd;i++) dados[i]=rand()%6+1;
 }
 
-// ================= Atacar =================
-void atacar(struct Continente mapa[], int jogadorAtual) {
-    int c1, p1, c2, p2;
+void mostrarDados(SDL_Renderer* r, TTF_Font* f, int atk[], int nAtk, int def[], int nDef){
+    SDL_Rect fundo = {0,500,800,100};
+    SDL_SetRenderDrawColor(r,10,10,10,255);
+    SDL_RenderFillRect(r,&fundo);
 
-    printf("\n--- SELECIONE O ATAQUE ---\n");
-    mostrarTerritoriosJogador(mapa, jogadorAtual);
+    char a[100]="ATK: ";
+    char d[100]="DEF: ";
 
-    printf("\nContinente atacante (numero): ");
-    scanf("%d", &c1);
-    printf("Pais atacante (numero): ");
-    scanf("%d", &p1);
+    for(int i=0;i<nAtk;i++){ char t[10]; sprintf(t,"%d ",atk[i]); strcat(a,t); }
+    for(int i=0;i<nDef;i++){ char t[10]; sprintf(t,"%d ",def[i]); strcat(d,t); }
 
-    printf("\nContinente defensor (numero): ");
-    scanf("%d", &c2);
-    printf("Pais defensor (numero): ");
-    scanf("%d", &p2);
-
-    if (c1 < 0 || c1 >= NUM_CONTINENTES || c2 < 0 || c2 >= NUM_CONTINENTES ||
-        p1 < 0 || p1 >= PAISES_POR_CONTINENTE || p2 < 0 || p2 >= PAISES_POR_CONTINENTE) {
-        printf("Escolha invalida!\n");
-        return;
-    }
-
-    struct Pais *atk = &mapa[c1].paises[p1];
-    struct Pais *def = &mapa[c2].paises[p2];
-
-    if (atk->dono != jogadorAtual) {
-        printf("Esse pais nao e seu!\n");
-        return;
-    }
-
-    if (atk->tropas <= 1) {
-        printf("Tropas insuficientes!\n");
-        return;
-    }
-
-    printf("\n%s atacando %s!\n", atk->nome, def->nome);
-
-    int dadosAtk[3], dadosDef[2];
-    int nAtk = (atk->tropas >= 3) ? 3 : atk->tropas - 1;
-    int nDef = (def->tropas >= 2) ? 2 : 1;
-
-    for (int i = 0; i < nAtk; i++) dadosAtk[i] = rand() % 6 + 1;
-    for (int i = 0; i < nDef; i++) dadosDef[i] = rand() % 6 + 1;
-
-    // ordenar
-    for (int i = 0; i < nAtk - 1; i++)
-        for (int j = i + 1; j < nAtk; j++)
-            if (dadosAtk[i] < dadosAtk[j]) {
-                int t = dadosAtk[i]; dadosAtk[i] = dadosAtk[j]; dadosAtk[j] = t;
-            }
-
-    for (int i = 0; i < nDef - 1; i++)
-        for (int j = i + 1; j < nDef; j++)
-            if (dadosDef[i] < dadosDef[j]) {
-                int t = dadosDef[i]; dadosDef[i] = dadosDef[j]; dadosDef[j] = t;
-            }
-
-    printf("\nDados Atacante: ");
-    for (int i = 0; i < nAtk; i++) printf("%d ", dadosAtk[i]);
-
-    printf("\nDados Defensor: ");
-    for (int i = 0; i < nDef; i++) printf("%d ", dadosDef[i]);
-    printf("\n");
-
-    int comp = (nAtk < nDef) ? nAtk : nDef;
-
-    for (int i = 0; i < comp; i++) {
-        if (dadosAtk[i] > dadosDef[i]) {
-            def->tropas--;
-            printf("Defensor perdeu 1 tropa\n");
-        } else {
-            atk->tropas--;
-            printf("Atacante perdeu 1 tropa\n");
-        }
-    }
-
-    if (def->tropas <= 0) {
-        printf("\n🏆 Territorio conquistado!\n");
-
-        def->dono = jogadorAtual;
-        def->tropas = 1;
-        atk->tropas--;
-    }
+    escreverTexto(r,f,a,50,520,(SDL_Color){255,80,80,255});
+    escreverTexto(r,f,d,50,550,(SDL_Color){80,150,255,255});
 }
 
-// ================= main =================
-int main() {
+int main(){
     srand(time(NULL));
+    SDL_Init(SDL_INIT_VIDEO);
+    TTF_Init();
 
-    struct Continente mapa[NUM_CONTINENTES] = {
-        {"America"}, {"Europa"}, {"Asia"}, {"Africa"}, {"Oceania"}
-    };
+    SDL_Window* w = SDL_CreateWindow("WAR SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,800,600,0);
+    SDL_Renderer* r = SDL_CreateRenderer(w,-1,SDL_RENDERER_ACCELERATED);
 
-    for (int i = 0; i < NUM_CONTINENTES; i++) {
-        for (int j = 0; j < PAISES_POR_CONTINENTE; j++) {
-            strcpy(mapa[i].paises[j].nome, nomesPaises[i][j]);
-        }
-    }
+    TTF_Font* font = TTF_OpenFont("arial.ttf",18);
 
-    int numJogadores;
+    int rodando=1, menu=1, numJogadores=2;
 
-    do {
-        printf("Quantos jogadores? (2 a 5): ");
-        scanf("%d", &numJogadores);
-    } while (numJogadores < 2 || numJogadores > MAX_JOGADORES);
+    SDL_Rect btnJogar={300,200,200,50};
+    SDL_Rect btnSairMenu={300,300,200,50};
+    SDL_Rect btnMais={520,200,40,40};
+    SDL_Rect btnMenos={240,200,40,40};
 
-    struct Jogador jogadores[MAX_JOGADORES];
+    // 🔥 CONFIGURAÇÃO ALINHADA DOS BOTÕES LADO DIREITO
+    int larguraBtn = 150;
+    int alturaBtn = 50;
+    int xBtn = 640; // Ajustado para centralizar melhor no painel de 620 a 800
+    
+    SDL_Rect btnAtacar   = {xBtn, 100, larguraBtn, alturaBtn};
+    SDL_Rect btnPular    = {xBtn, 170, larguraBtn, alturaBtn}; // 100 + 50 + 20 de espaço
+    SDL_Rect btnSairJogo = {xBtn, 240, larguraBtn, alturaBtn}; // 170 + 50 + 20 de espaço
 
-    for (int i = 0; i < numJogadores; i++) {
-        printf("Nome do jogador %d: ", i);
-        scanf("%s", jogadores[i].nome);
-    }
+    Jogador jogadores[MAX_JOGADORES];
+    SDL_Color cores[]={{255,50,50,255},{50,50,255,255},{50,255,50,255},{255,255,50,255},{200,50,250,255}};
 
-    int totalPaises = NUM_CONTINENTES * PAISES_POR_CONTINENTE;
-    int donos[25];
+    Pais mapa[TOTAL_PAISES];
+    const char* nomes[]={"Brasil","EUA","China","Russia","Alemanha","Japao","India","Egito","Franca","Italia",
+    "Canada","Mexico","Australia","Argentina","Chile","Peru","Angola","Grecia","Turquia","Ira",
+    "Iraque","Coreia","Espanha","Suecia","Congo"};
 
-    for (int i = 0; i < totalPaises; i++)
-        donos[i] = i % numJogadores;
+    Pais* selecionado=NULL;
+    int modoAtaque=0, turno=0;
 
-    for (int i = totalPaises - 1; i > 0; i--) {
-        int j = rand() % (i + 1);
-        int temp = donos[i];
-        donos[i] = donos[j];
-        donos[j] = temp;
-    }
+    int dadosAtk[3], dadosDef[3], nAtk=0, nDef=0;
 
-    int index = 0;
+    int tropasDisponiveis=0;
+    int tropasCalculadas=0;
 
-    for (int i = 0; i < NUM_CONTINENTES; i++) {
-        for (int j = 0; j < PAISES_POR_CONTINENTE; j++) {
-            mapa[i].paises[j].dono = donos[index++];
-            mapa[i].paises[j].tropas = 3;
-        }
-    }
+    while(rodando){
+        SDL_Event ev;
+        while(SDL_PollEvent(&ev)){
+            if(ev.type==SDL_QUIT) rodando=0;
 
-    int turno = 0;
+            if(ev.type==SDL_MOUSEBUTTONDOWN){
+                int mx=ev.button.x,my=ev.button.y;
 
-    while (1) {
-        int jogadorAtual = turno % numJogadores;
+                if(menu){
+                    if(mouseSobre(btnJogar,mx,my)){
+                        for(int i=0;i<numJogadores;i++){
+                            sprintf(jogadores[i].nome,"Jogador %d",i+1);
+                            jogadores[i].cor=cores[i];
+                        }
+                        for(int i=0;i<TOTAL_PAISES;i++){
+                            strcpy(mapa[i].nome,nomes[i]);
+                            mapa[i].dono=i%numJogadores;
+                            mapa[i].tropas=3;
+                            mapa[i].retangulo=(SDL_Rect){20+(i%5)*120,20+(i/5)*80,120,60};
+                        }
+                        turno=0; menu=0; tropasCalculadas=0;
+                    }
+                    if(mouseSobre(btnSairMenu,mx,my)) rodando=0;
+                    if(mouseSobre(btnMais,mx,my) && numJogadores<5) numJogadores++;
+                    if(mouseSobre(btnMenos,mx,my) && numJogadores>2) numJogadores--;
+                }
+                else{
+                    int id=turno%numJogadores;
+                    if(mouseSobre(btnAtacar,mx,my)){modoAtaque=1; selecionado=NULL;}
+                    if(mouseSobre(btnPular,mx,my)){
+                        modoAtaque=0; turno++; selecionado=NULL; tropasCalculadas=0;
+                    }
+                    if(mouseSobre(btnSairJogo,mx,my)){menu=1;}
 
-        printf("\n========================\n");
-        printf("VEZ DE %s\n", jogadores[jogadorAtual].nome);
-        printf("========================\n");
+                    for(int i=0;i<TOTAL_PAISES;i++){
+                        Pais* p=&mapa[i];
+                        if(mouseSobre(p->retangulo,mx,my)){
+                            if(modoAtaque){
+                                if(!selecionado && p->dono==id && p->tropas>1) selecionado=p;
+                                else if(selecionado && p->dono!=id){
+                                    nAtk = selecionado->tropas>3?3:selecionado->tropas-1;
+                                    nDef = p->tropas>2?3:p->tropas;
+                                    rolarDados(dadosAtk,nAtk);
+                                    rolarDados(dadosDef,nDef);
 
-        mostrarPorJogador(mapa, jogadores, numJogadores);
-        mostrarMapa(mapa, jogadores);
+                                    for(int a=0;a<nAtk-1;a++)
+                                        for(int b=a+1;b<nAtk;b++)
+                                            if(dadosAtk[a]<dadosAtk[b]){ int t=dadosAtk[a]; dadosAtk[a]=dadosAtk[b]; dadosAtk[b]=t; }
 
-        printf("\n1 - Atacar\n0 - Pular\n");
-        int op;
-        scanf("%d", &op);
+                                    for(int a=0;a<nDef-1;a++)
+                                        for(int b=a+1;b<nDef;b++)
+                                            if(dadosDef[a]<dadosDef[b]){ int t=dadosDef[a]; dadosDef[a]=dadosDef[b]; dadosDef[b]=t; }
 
-        if (op == 1)
-            atacar(mapa, jogadorAtual);
-
-        int dono = mapa[0].paises[0].dono;
-        int venceu = 1;
-
-        for (int i = 0; i < NUM_CONTINENTES; i++) {
-            for (int j = 0; j < PAISES_POR_CONTINENTE; j++) {
-                if (mapa[i].paises[j].dono != dono) {
-                    venceu = 0;
+                                    int min = nAtk<nDef?nAtk:nDef;
+                                    for(int d=0;d<min;d++){
+                                        if(dadosAtk[d]>dadosDef[d]) p->tropas--;
+                                        else selecionado->tropas--;
+                                    }
+                                    if(p->tropas<=0){ p->dono=id; p->tropas=1; selecionado->tropas--; }
+                                    selecionado=NULL;
+                                }
+                            }
+                            else if(p->dono==id && tropasDisponiveis>0){
+                                p->tropas++; tropasDisponiveis--;
+                            }
+                        }
+                    }
                 }
             }
         }
 
-        if (venceu) {
-            printf("\n🏆 %s VENCEU O JOGO!\n", jogadores[dono].nome);
-            break;
+        SDL_SetRenderDrawColor(r,20,20,20,255);
+        SDL_RenderClear(r);
+
+        int mx,my;
+        SDL_GetMouseState(&mx,&my);
+
+        if(menu){
+            SDL_SetRenderDrawColor(r,0,200,0,255);
+            SDL_RenderFillRect(r,&btnJogar);
+            SDL_SetRenderDrawColor(r,200,0,0,255);
+            SDL_RenderFillRect(r,&btnSairMenu);
+
+            SDL_SetRenderDrawColor(r, mouseSobre(btnMais,mx,my)?255:200,255,100,255);
+            SDL_RenderFillRect(r,&btnMais);
+            SDL_SetRenderDrawColor(r, mouseSobre(btnMenos,mx,my)?255:200,255,100,255);
+            SDL_RenderFillRect(r,&btnMenos);
+
+            escreverTexto(r,font,"JOGAR",360,215,(SDL_Color){255,255,255,255});
+            escreverTexto(r,font,"SAIR",370,315,(SDL_Color){255,255,255,255});
+            escreverTexto(r,font,"+",btnMais.x+10,btnMais.y,(SDL_Color){0,0,0,255});
+            escreverTexto(r,font,"-",btnMenos.x+12,btnMenos.y,(SDL_Color){0,0,0,255});
+
+            char buf[50];
+            sprintf(buf,"Jogadores: %d",numJogadores);
+            escreverTexto(r,font,buf,330,150,(SDL_Color){255,255,0,255});
         }
+        else{
+            int id=turno%numJogadores;
+            if(!tropasCalculadas){
+                int territ=0;
+                for(int i=0;i<TOTAL_PAISES;i++) if(mapa[i].dono==id) territ++;
+                tropasDisponiveis = territ/2;
+                if(tropasDisponiveis<1) tropasDisponiveis=1;
+                tropasCalculadas=1;
+            }
 
-        turno++;
+            SDL_Rect painel = {620,0,180,600};
+            SDL_SetRenderDrawColor(r,30,30,30,255);
+            SDL_RenderFillRect(r,&painel);
+
+            for(int i=0;i<TOTAL_PAISES;i++){
+                Pais* p=&mapa[i];
+                SDL_SetRenderDrawColor(r, jogadores[p->dono].cor.r, jogadores[p->dono].cor.g, jogadores[p->dono].cor.b,255);
+                SDL_RenderFillRect(r,&p->retangulo);
+                SDL_SetRenderDrawColor(r,255,255,255,255);
+                SDL_RenderDrawRect(r,&p->retangulo);
+                char txt[40];
+                sprintf(txt,"%s (%d)",p->nome,p->tropas);
+                escreverTexto(r,font,txt,p->retangulo.x+5,p->retangulo.y+5,(SDL_Color){255,255,255,255});
+            }
+
+            char hud[80];
+            sprintf(hud,"Vez: %s | Tropas: %d | %s", jogadores[id].nome, tropasDisponiveis, modoAtaque?"ATAQUE":"REFORCO");
+            escreverTexto(r,font,hud,20,460,(SDL_Color){255,255,255,255});
+
+            // 🔥 DESENHO DOS BOTÕES LADO DIREITO COM HOVER
+            SDL_SetRenderDrawColor(r, mouseSobre(btnAtacar,mx,my)?150:100, 100, 100, 255);
+            SDL_RenderFillRect(r,&btnAtacar);
+            
+            SDL_SetRenderDrawColor(r, mouseSobre(btnPular,mx,my)?150:100, 100, 100, 255);
+            SDL_RenderFillRect(r,&btnPular);
+            
+            SDL_SetRenderDrawColor(r, mouseSobre(btnSairJogo,mx,my)?150:100, 100, 100, 255);
+            SDL_RenderFillRect(r,&btnSairJogo);
+
+            // TEXTOS DOS BOTÕES (Ajustados para o novo tamanho)
+            escreverTexto(r,font,"Atacar",btnAtacar.x+45,btnAtacar.y+15,(SDL_Color){255,255,255,255});
+            escreverTexto(r,font,"Pular",btnPular.x+50,btnPular.y+15,(SDL_Color){255,255,255,255});
+            escreverTexto(r,font,"Sair",btnSairJogo.x+55,btnSairJogo.y+15,(SDL_Color){255,255,255,255});
+
+            if(nAtk>0) mostrarDados(r,font,dadosAtk,nAtk,dadosDef,nDef);
+        }
+        SDL_RenderPresent(r);
     }
-
+    SDL_Quit();
     return 0;
 }

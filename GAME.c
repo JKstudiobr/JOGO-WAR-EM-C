3d9fca4479932ba2018f0f265b1cb80080f7612f
Studio JK <devstudiojk@gmail.com>
2026-04-07 05:52:29 -0300

    AQUI JA DA PRA JOGAR COM AMIGO, MECANICA 100%
    sendo apenas feio , o jogo ja simula quase certo o jogo!

--- GAME.c
+++ GAME.c
@@ -6,7 +6,7 @@
 #include <time.h>
 #include <string.h>
 
-#define TOTAL_PAISES 25
+#define TOTAL_PAISES 50
 #define MAX_JOGADORES 5
 
 typedef struct {
@@ -71,22 +71,27 @@ int main(){
     SDL_Rect btnMais={520,200,40,40};
     SDL_Rect btnMenos={240,200,40,40};
 
-    // 🔥 CONFIGURAÇÃO ALINHADA DOS BOTÕES LADO DIREITO
     int larguraBtn = 150;
     int alturaBtn = 50;
-    int xBtn = 640; // Ajustado para centralizar melhor no painel de 620 a 800
-    
+    int xBtn = 640;
+
     SDL_Rect btnAtacar   = {xBtn, 100, larguraBtn, alturaBtn};
-    SDL_Rect btnPular    = {xBtn, 170, larguraBtn, alturaBtn}; // 100 + 50 + 20 de espaço
-    SDL_Rect btnSairJogo = {xBtn, 240, larguraBtn, alturaBtn}; // 170 + 50 + 20 de espaço
+    SDL_Rect btnPular    = {xBtn, 170, larguraBtn, alturaBtn};
+    SDL_Rect btnSairJogo = {xBtn, 240, larguraBtn, alturaBtn};
 
     Jogador jogadores[MAX_JOGADORES];
     SDL_Color cores[]={{255,50,50,255},{50,50,255,255},{50,255,50,255},{255,255,50,255},{200,50,250,255}};
 
     Pais mapa[TOTAL_PAISES];
-    const char* nomes[]={"Brasil","EUA","China","Russia","Alemanha","Japao","India","Egito","Franca","Italia",
+
+    const char* nomes[]={
+    "Brasil","EUA","China","Russia","Alemanha","Japao","India","Egito","Franca","Italia",
     "Canada","Mexico","Australia","Argentina","Chile","Peru","Angola","Grecia","Turquia","Ira",
-    "Iraque","Coreia","Espanha","Suecia","Congo"};
+    "Iraque","Coreia","Espanha","Suecia","Congo",
+    "Noruega","Finlandia","Polonia","Ucrania","Portugal","Holanda","Belgica","Suica","Austria","Hungria",
+    "Dinamarca","Irlanda","Escocia","NovaZelandia","Indonesia","Filipinas","Vietna","Tailandia","Marrocos","Argelia",
+    "Nigeria","Quenia","Etiopia","Colombia","Venezuela"
+    };
 
     Pais* selecionado=NULL;
     int modoAtaque=0, turno=0;
@@ -114,7 +119,7 @@ int main(){
                             strcpy(mapa[i].nome,nomes[i]);
                             mapa[i].dono=i%numJogadores;
                             mapa[i].tropas=3;
-                            mapa[i].retangulo=(SDL_Rect){20+(i%5)*120,20+(i/5)*80,120,60};
+                            mapa[i].retangulo=(SDL_Rect){20+(i%5)*120,20+(i/5)*40,120,40};
                         }
                         turno=0; menu=0; tropasCalculadas=0;
                     }
@@ -124,42 +129,73 @@ int main(){
                 }
                 else{
                     int id=turno%numJogadores;
+
                     if(mouseSobre(btnAtacar,mx,my)){modoAtaque=1; selecionado=NULL;}
-                    if(mouseSobre(btnPular,mx,my)){
-                        modoAtaque=0; turno++; selecionado=NULL; tropasCalculadas=0;
-                    }
+                    if(mouseSobre(btnPular,mx,my)){modoAtaque=0; turno++; selecionado=NULL; tropasCalculadas=0;}
                     if(mouseSobre(btnSairJogo,mx,my)){menu=1;}
 
                     for(int i=0;i<TOTAL_PAISES;i++){
                         Pais* p=&mapa[i];
+
                         if(mouseSobre(p->retangulo,mx,my)){
                             if(modoAtaque){
-                                if(!selecionado && p->dono==id && p->tropas>1) selecionado=p;
+
+                                if(!selecionado && p->dono==id && p->tropas>1)
+                                    selecionado=p;
+
                                 else if(selecionado && p->dono!=id){
-                                    nAtk = selecionado->tropas>3?3:selecionado->tropas-1;
-                                    nDef = p->tropas>2?3:p->tropas;
+
+                                    // Determina quantidade de dados
+                                    nAtk = (selecionado->tropas > 3) ? 3 : selecionado->tropas - 1;
+                                    nDef = (p->tropas >= 3) ? 3 : p->tropas;
+
                                     rolarDados(dadosAtk,nAtk);
                                     rolarDados(dadosDef,nDef);
 
+                                    // Ordenar Atacante (Maior para Menor)
                                     for(int a=0;a<nAtk-1;a++)
                                         for(int b=a+1;b<nAtk;b++)
-                                            if(dadosAtk[a]<dadosAtk[b]){ int t=dadosAtk[a]; dadosAtk[a]=dadosAtk[b]; dadosAtk[b]=t; }
+                                            if(dadosAtk[a]<dadosAtk[b]){
+                                                int t=dadosAtk[a]; dadosAtk[a]=dadosAtk[b]; dadosAtk[b]=t;
+                                            }
 
+                                    // Ordenar Defensor (Maior para Menor)
                                     for(int a=0;a<nDef-1;a++)
                                         for(int b=a+1;b<nDef;b++)
-                                            if(dadosDef[a]<dadosDef[b]){ int t=dadosDef[a]; dadosDef[a]=dadosDef[b]; dadosDef[b]=t; }
+                                            if(dadosDef[a]<dadosDef[b]){
+                                                int t=dadosDef[a]; dadosDef[a]=dadosDef[b]; dadosDef[b]=t;
+                                            }
 
-                                    int min = nAtk<nDef?nAtk:nDef;
-                                    for(int d=0;d<min;d++){
-                                        if(dadosAtk[d]>dadosDef[d]) p->tropas--;
-                                        else selecionado->tropas--;
+                                    // --- LÓGICA DE COMBATE CORRIGIDA ---
+                                    int comparacoes = (nAtk < nDef) ? nAtk : nDef;
+
+                                    // 1. Compara os dados que possuem par
+                                    for(int j=0; j<comparacoes; j++){
+                                        if(dadosAtk[j] > dadosDef[j])
+                                            p->tropas--;
+                                        else
+                                            selecionado->tropas--;
                                     }
-                                    if(p->tropas<=0){ p->dono=id; p->tropas=1; selecionado->tropas--; }
-                                    selecionado=NULL;
+
+                                    // 2. Dados excedentes (Caso o Atacante tenha mais dados que o Defensor)
+                                    if(nAtk > nDef && p->tropas > 0){
+                                        int extras = nAtk - nDef;
+                                        p->tropas -= extras;
+                                    }
+
+                                    // 3. Verifica conquista de território
+                                    if(p->tropas <= 0){
+                                        p->dono = id;
+                                        p->tropas = 1;         // Ocupa com 1 tropa
+                                        selecionado->tropas--; // Tira 1 da origem para mover
+                                    }
+
+                                    selecionado=NULL; 
                                 }
                             }
                             else if(p->dono==id && tropasDisponiveis>0){
-                                p->tropas++; tropasDisponiveis--;
+                                p->tropas++;
+                                tropasDisponiveis--;
                             }
                         }
                     }
@@ -176,6 +212,7 @@ int main(){
         if(menu){
             SDL_SetRenderDrawColor(r,0,200,0,255);
             SDL_RenderFillRect(r,&btnJogar);
+
             SDL_SetRenderDrawColor(r,200,0,0,255);
             SDL_RenderFillRect(r,&btnSairMenu);
 
@@ -195,9 +232,12 @@ int main(){
         }
         else{
             int id=turno%numJogadores;
+
             if(!tropasCalculadas){
                 int territ=0;
-                for(int i=0;i<TOTAL_PAISES;i++) if(mapa[i].dono==id) territ++;
+                for(int i=0;i<TOTAL_PAISES;i++)
+                    if(mapa[i].dono==id) territ++;
+
                 tropasDisponiveis = territ/2;
                 if(tropasDisponiveis<1) tropasDisponiveis=1;
                 tropasCalculadas=1;
@@ -209,38 +249,51 @@ int main(){
 
             for(int i=0;i<TOTAL_PAISES;i++){
                 Pais* p=&mapa[i];
-                SDL_SetRenderDrawColor(r, jogadores[p->dono].cor.r, jogadores[p->dono].cor.g, jogadores[p->dono].cor.b,255);
+
+                SDL_SetRenderDrawColor(r,
+                    jogadores[p->dono].cor.r,
+                    jogadores[p->dono].cor.g,
+                    jogadores[p->dono].cor.b,255);
+
                 SDL_RenderFillRect(r,&p->retangulo);
+
                 SDL_SetRenderDrawColor(r,255,255,255,255);
                 SDL_RenderDrawRect(r,&p->retangulo);
+
                 char txt[40];
                 sprintf(txt,"%s (%d)",p->nome,p->tropas);
                 escreverTexto(r,font,txt,p->retangulo.x+5,p->retangulo.y+5,(SDL_Color){255,255,255,255});
             }
 
             char hud[80];
-            sprintf(hud,"Vez: %s | Tropas: %d | %s", jogadores[id].nome, tropasDisponiveis, modoAtaque?"ATAQUE":"REFORCO");
+            sprintf(hud,"Vez: %s | Tropas: %d | %s",
+                jogadores[id].nome,
+                tropasDisponiveis,
+                modoAtaque?"ATAQUE":"REFORCO");
+
             escreverTexto(r,font,hud,20,460,(SDL_Color){255,255,255,255});
 
-            // 🔥 DESENHO DOS BOTÕES LADO DIREITO COM HOVER
-            SDL_SetRenderDrawColor(r, mouseSobre(btnAtacar,mx,my)?150:100, 100, 100, 255);
+            SDL_SetRenderDrawColor(r, mouseSobre(btnAtacar,mx,my)?150:100,100,100,255);
             SDL_RenderFillRect(r,&btnAtacar);
-            
-            SDL_SetRenderDrawColor(r, mouseSobre(btnPular,mx,my)?150:100, 100, 100, 255);
+
+            SDL_SetRenderDrawColor(r, mouseSobre(btnPular,mx,my)?150:100,100,100,255);
             SDL_RenderFillRect(r,&btnPular);
-            
-            SDL_SetRenderDrawColor(r, mouseSobre(btnSairJogo,mx,my)?150:100, 100, 100, 255);
+
+            SDL_SetRenderDrawColor(r, mouseSobre(btnSairJogo,mx,my)?150:100,100,100,255);
             SDL_RenderFillRect(r,&btnSairJogo);
 
-            // TEXTOS DOS BOTÕES (Ajustados para o novo tamanho)
             escreverTexto(r,font,"Atacar",btnAtacar.x+45,btnAtacar.y+15,(SDL_Color){255,255,255,255});
             escreverTexto(r,font,"Pular",btnPular.x+50,btnPular.y+15,(SDL_Color){255,255,255,255});
             escreverTexto(r,font,"Sair",btnSairJogo.x+55,btnSairJogo.y+15,(SDL_Color){255,255,255,255});
 
-            if(nAtk>0) mostrarDados(r,font,dadosAtk,nAtk,dadosDef,nDef);
+            if(nAtk>0)
+                mostrarDados(r,font,dadosAtk,nAtk,dadosDef,nDef);
         }
+
         SDL_RenderPresent(r);
     }
+
     SDL_Quit();
+    TTF_Quit();
     return 0;
 }
